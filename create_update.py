7dc16e3b106a519d870dda46167dee430249cec7
Edward Sargsyan <edward.sarkisyan@gmail.com>
2020-12-03 18:03:53 +0100

    Update script generator

---
+++ create_update.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python2
+
+import sys
+
+name = sys.argv[1]
+version = sys.argv[2]
+postfix = sys.argv[3]
+host = sys.argv[4]
+location = sys.argv[5]
+
+template = """<update>
+    <version>$version</version>
+    <name>$name</name>
+    <url>$host/aquabox-$postfix.apk</url>
+</update>"""
+
+template = template.replace("$name", name).replace("$version", version).replace("$postfix", postfix).replace("$host", host)
+
+
+
+f = open(location + "/aquabox-" + postfix + ".xml", 'w')
+f.write(template)
+f.close()
+print(template)
