94ab33505ffada51031df95c572c0950e153610d
Eduard Sargsyan <edward.sarkisyan@gmail.com>
2022-05-25 23:45:59 +0200

    Added timeouts for requests

--- src/app/aquabox-api.ts
+++ src/app/aquabox-api.ts
@@ -27,17 +27,35 @@ export class AquaboxAPI {
         return [this.localUrl + url, this.cloudUrl + url]
     }
 
+    private timeouts() {
+        switch (this.box.connected) {
+            case ConnectionMethods.Both: {
+                return [2 * 60 * 1000, 5 * 1000]
+            }
+            case ConnectionMethods.CloudOnly: {
+                return [2 * 60 * 1000, 2 * 60 * 1000]
+            }
+            case ConnectionMethods.LocalOnly: {
+                return [5 * 1000, 5 * 1000]
+            }
+            default: {
+                
+            }
+        }
+        return [5 * 1000, 2 * 60 * 1000]
+    }
+
     public get(url: string,
         request: HttpHeaders,
         response: (box: Aquabox, data: Object) => void,
         fail: (box: Aquabox, error: any) => void) {
         this.http.get<Object>(this.urls(url)[0], { headers: request })
-            .pipe(timeout(5000))
+            .pipe(timeout(this.timeouts()[0]))
             .subscribe((data) => {
                 response(this.box, data)
             }, (error) => {
-                console.error("Local GET request to " + this.urls(url)[0] + " failed with " + error);
                 this.http.get<Object>(this.urls(url)[1], { headers: request })
+                    .pipe(timeout(this.timeouts()[1]))
                     .subscribe((data) => {
                         response(this.box, data)
                     }, (error) => {
@@ -51,12 +69,12 @@ export class AquaboxAPI {
         response: (box: Aquabox, data: Object) => void,
         fail: (box: Aquabox, error: any) => void) {
         this.http.delete<Object>(this.urls(url)[0], { headers: request })
-            .pipe(timeout(30000))
+            .pipe(timeout(this.timeouts()[0]))
             .subscribe((data) => {
                 response(this.box, data)
             }, (error) => {
-                console.error("Local request DELETE to " + this.urls(url)[0] + " failed with " + error);
                 this.http.delete<Object>(this.urls(url)[1], { headers: request })
+                    .pipe(timeout(this.timeouts()[1]))
                     .subscribe((data) => {
                         response(this.box, data)
                     }, (error) => {
@@ -71,12 +89,12 @@ export class AquaboxAPI {
         response: (box: Aquabox, data: Object) => void,
         fail: (box: Aquabox, error: any) => void) {
         this.http.post<Object>(this.urls(url)[0], data, { headers: request })
-            .pipe(timeout(30000))
+            .pipe(timeout(this.timeouts()[0]))
             .subscribe((data) => {
                 response(this.box, data)
             }, (error) => {
-                console.error("Local request POST to " + this.urls(url)[0] + " failed with " + error);
                 this.http.post<Object>(this.urls(url)[1], data, { headers: request })
+                    .pipe(timeout(this.timeouts()[1]))
                     .subscribe((data) => {
                         response(this.box, data)
                     }, (error) => {
@@ -91,12 +109,12 @@ export class AquaboxAPI {
         response: (box: Aquabox, data: Object) => void,
         fail: (box: Aquabox, error: any) => void) {
         this.http.put<Object>(this.urls(url)[0], data, { headers: request })
-            .pipe(timeout(5000))
+            .pipe(timeout(this.timeouts()[0]))
             .subscribe((data) => {
                 response(this.box, data)
             }, (error) => {
-                console.error("Local request PUT to " + this.urls(url)[0] + " failed with " + error);
                 this.http.put<Object>(this.urls(url)[1], data, { headers: request })
+                    .pipe(timeout(this.timeouts()[1]))
                     .subscribe((data) => {
                         response(this.box, data)
                     }, (error) => {
