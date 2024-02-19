7d4f071b024143a983cfc4fa54dbb48a21311208
Edward <edward.sarkisyan@gmail.com>
2024-02-19 23:30:25 +0100

    Windows Docker run, and building Android apk

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
-export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
-export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
 
-#export JAVA_HOME=/home/esargsyan/ionictests/android-studio/jre
-#export CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL=https://services.gradle.org/distributions/gradle-4.10.1-bin.zip
-#export PATH=$PATH:$JAVA_HOME/../gradle/gradle-5.1.1/bin
+#export ANDROID_HOME=$HOME/Android/Sdk
+#export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
+#export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
+#export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
 
-ionic cordova run android -l --no-interactive --verbose  --port=8200
-#ionic cordova app:ionic-cordova-build --platform=android
+ionic cordova build android $CORDOVA_EX_ARGS
--- run_ionic_env.bat
+++ run_ionic_env.bat
@@ -3,4 +3,4 @@ set SCRIPT_DIR=%CD%
 popd
 
 
-docker run -p 8100:8100   -it -v %SCRIPT_DIR%:/ionicapp kusumoto/docker-ionic-android-sdk /bin/bash
+docker run -p 8100:8100 -it -v %SCRIPT_DIR%:/ionicapp kusumoto/docker-ionic-android-sdk /bin/bash
