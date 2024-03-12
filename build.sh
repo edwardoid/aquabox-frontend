b347817788f77fdc07abbdcb47562008091c8c6a
Areg <areg@hylink.am>
2024-03-12 19:53:27 +0400

    upgrade tools to the latest versions

--- build.sh
+++ build.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-export ANDROID_HOME=/opt/android-sdk
+export ANDROID_HOME=/usr/lib/android-sdk
 export ANDROID_SDK_ROOT=$ANDROID_HOME
-export PATH=$PATH:$ANDROID_SDK_ROOT/tools/bin
+export PATH=$PATH:$ANDROID_SDK_ROOT/cmdline-tools/latest/bin
 export PATH=$PATH:$ANDROID_SDK_ROOT/platform-tools
-export PATH=$PATH:$ANDROID_SDK_ROOT/emulator
-
-#export JAVA_HOME=/home/esargsyan/ionictests/android-studio/jre
-#export CORDOVA_ANDROID_GRADLE_DISTRIBUTION_URL=https://services.gradle.org/distributions/gradle-4.10.1-bin.zip
-#export PATH=$PATH:$JAVA_HOME/../gradle/gradle-5.1.1/bin
-
-#ionic cordova run android -l --no-interactive --verbose  --port=8200
-#ionic cordova app:ionic-cordova-build --platform=android
-#ionic cordova build android --verbose
 
 # install modules refered in package.json
-npm install
+npm install --force
 
 # build www folder
 npm run build
